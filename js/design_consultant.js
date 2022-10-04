9e8a9cdd768e7f509db1edf8ac3511051ad27fbb
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2022-10-04 20:10:35 +0300

    adsbgg

---
+++ js/design_consultant.js
@@ -0,0 +1,6 @@
+const openBlockButton = document.querySelector('.open_block_design');
+
+openBlockButton.addEventListener('click',() => {
+    document.querySelector('.heden__design').style.display = 'block';
+    document.querySelector('.hiden__button_design').style.display = 'none';
+})
