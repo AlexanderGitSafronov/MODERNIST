c2b7ff38f8bcfa6c9d6c13323c40fad6d4c148d2
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2022-11-08 11:08:07 +0200

    iirroo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+$(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+} )
+
+
 // КЛИК ПО ЛАЙКУ
 // var tableElements = document.querySelectorAll(".like");
 
